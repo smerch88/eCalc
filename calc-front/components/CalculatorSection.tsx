096e77820fbf80234c6b3dae0f27dd476ff2b086
smerch88 <maxarmyk@gmail.com>
2024-11-13 21:03:52 +0200

    fix

--- calc-front/components/CalculatorSection.tsx
+++ calc-front/components/CalculatorSection.tsx
@@ -3,7 +3,7 @@ export const CalculatorSection = () => {
     <section>
       <div className="container py-10">
         <h2 className="text-4xl font-semibold pb-6">Ваш калькулятор</h2>
-        <p className="text-2x mb-12">
+        <p className="text-2xl mb-12">
           Обирайте прилад, який хочете розрахувати порівнюючи з центральною
           мережею та визначайте, що для вас є вигідніше.
         </p>
