8b36d0c077387acde693edab0f7a164f77ba9718
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-12-14 22:41:02 +0200

    create laundry washer calculator

--- calc-front/components/TariffChange.tsx
+++ calc-front/components/TariffChange.tsx
@@ -1,9 +1,21 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent } from "react";
 
 interface Option {
     value: string;
     label: string;
     tariff: string;
+    icon?: JSX.Element | JSX.Element[];
+}
+
+interface efficiencyOptions {
+    value: string;
+    label: string;
+    icon?: JSX.Element | JSX.Element[];
+}
+
+interface loadSizeOptions {
+    value: string;
+    label: string;
     icon?: JSX.Element | JSX.Element[];
 }
 
@@ -17,6 +29,8 @@ interface TariffChangeFormData {
 interface TariffChangeProps<T extends TariffChangeFormData> {
     selectedCostPerKWh: string;
     setSelectedCostPerKWh: (value: string) => void;
+    setSelectedEfficiencyClass?: (value: string) => void;
+    setSelectedLoadSize?: (value: string) => void;
     formData: T;
     setFormData: React.Dispatch<React.SetStateAction<T>>;
     setIsValid: (value: boolean) => void;
@@ -33,7 +47,12 @@ export const icons = {
             xmlns="http://www.w3.org/2000/svg"
         >
             <circle cx="9" cy="10" r="3.4" stroke="#191919" strokeWidth="1.2" />
-            <path d="M9.04102 4.53577V1" stroke="#191919" strokeWidth="1.2" strokeLinecap="round" />
+            <path
+                d="M9.04102 4.53577V1"
+                stroke="#191919"
+                strokeWidth="1.2"
+                strokeLinecap="round"
+            />
             <path
                 d="M9.04102 18.9999V15.4641"
                 stroke="#191919"
@@ -94,7 +113,11 @@ export const icons = {
                 strokeWidth="1.2"
                 strokeLinecap="round"
             />
-            <path d="M5.83754 5.42736L4.86023 3.89282" stroke="#191919" strokeLinecap="round" />
+            <path
+                d="M5.83754 5.42736L4.86023 3.89282"
+                stroke="#191919"
+                strokeLinecap="round"
+            />
         </svg>
     ),
     night: (
@@ -124,23 +147,67 @@ export const icons = {
 
 export const options: Option[] = [
     {
-        value: 'single-zone',
-        label: 'Однозонний',
-        tariff: '4.32',
+        value: "single-zone",
+        label: "Однозонний",
+        tariff: "4.32",
         icon: icons.day,
     },
     {
-        value: 'two-zone',
-        label: 'Двозонний',
-        tariff: '4.32         2.16',
+        value: "two-zone",
+        label: "Двозонний",
+        tariff: "4.32         2.16",
         icon: [icons.night, icons.day],
     },
-    { value: 'three-zone', label: 'Ввести вручну', tariff: '' },
+    { value: "three-zone", label: "Ввести вручну", tariff: "" },
+];
+
+export const efficiencyOptions: efficiencyOptions[] = [
+    {
+        value: "A+++",
+        label: "A+++",
+    },
+    {
+        value: "A++",
+        label: "A++",
+    },
+    {
+        value: "A+",
+        label: "A+",
+    },
+    {
+        value: "A",
+        label: "A",
+    },
+    {
+        value: "B",
+        label: "B",
+    },
+    {
+        value: "C",
+        label: "C",
+    },
+];
+
+export const loadSizeOptions: loadSizeOptions[] = [
+    {
+        value: "small",
+        label: "Малий об'єм завантажень",
+    },
+    {
+        value: "medium",
+        label: "Середній об'єм завантажень",
+    },
+    {
+        value: "large",
+        label: "Великий об'єм завантажень",
+    },
 ];
 
 export const TariffChange = <T extends TariffChangeFormData>({
     selectedCostPerKWh,
     setSelectedCostPerKWh,
+    setSelectedEfficiencyClass,
+    setSelectedLoadSize,
     formData,
     setFormData,
     setIsValid,
@@ -148,25 +215,25 @@ export const TariffChange = <T extends TariffChangeFormData>({
 }: TariffChangeProps<T>) => {
     const handleTariffChange = (event: ChangeEvent<HTMLSelectElement>): void => {
         const selectedOption = options.find(option => option.value === event.target.value);
-        const tariffValue = selectedOption?.tariff || '';
+        const tariffValue = selectedOption?.tariff || "";
         const selectedIcon = selectedOption?.icon;
 
         setSelectedCostPerKWh(event.target.value);
         setIsValid(true);
-        setErrorMessage('');
+        setErrorMessage("");
 
-        if (event.target.value === 'two-zone' && tariffValue) {
+        if (event.target.value === "two-zone" && tariffValue) {
             const [dayRate, nightRate] = tariffValue.split(/\s+/).map(parseFloat);
             const nightRateFactor = nightRate / dayRate;
 
-            setFormData(prev => ({
+            setFormData((prev) => ({
                 ...prev,
                 costPerKWh: tariffValue,
                 nightRateFactor,
                 icon: selectedIcon,
             }));
         } else {
-            setFormData(prev => ({
+            setFormData((prev) => ({
                 ...prev,
                 costPerKWh: tariffValue,
                 nightRateFactor: 1,
@@ -175,28 +242,52 @@ export const TariffChange = <T extends TariffChangeFormData>({
         }
     };
 
+    const handleEfficiencyChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setSelectedEfficiencyClass?.(event.target.value);
+        setIsValid(true);
+        setErrorMessage("");
+
+        const selectedOption = event.target.value;
+        setFormData((prev) => ({
+            ...prev,
+            efficiencyClass: selectedOption,
+        }));
+    };
+
+    const handleLoadSizeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setSelectedLoadSize?.(event.target.value);
+        setIsValid(true);
+        setErrorMessage("");
+
+        const selectedOption = event.target.value;
+        setFormData((prev) => ({
+            ...prev,
+            loadSize: selectedOption,
+        }));
+    };
+
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
         const { id, value } = event.target;
 
         setIsValid(true);
-        setErrorMessage('');
+        setErrorMessage("");
 
-        if (selectedCostPerKWh !== 'three-zone') {
-            setFormData(prev => ({ ...prev, [id]: value }));
+        if (selectedCostPerKWh !== "three-zone") {
+            setFormData((prev) => ({ ...prev, [id]: value }));
             return;
         }
 
-        setFormData(prev => ({ ...prev, costPerKWh: value }));
+        setFormData((prev) => ({ ...prev, costPerKWh: value }));
 
         const isNumberValid = /^(?!$)(\d+(\.\d{0,2})?)$/.test(value);
         if (!isNumberValid) {
             setIsValid(false);
-            setErrorMessage('Введіть число до двох знаків після коми.');
+            setErrorMessage("Введіть число до двох знаків після коми.");
         }
     };
 
     const getCalculationValue = (): number => {
-        if (selectedCostPerKWh === 'two-zone') {
+        if (selectedCostPerKWh === "two-zone") {
             const tariffValues = formData.costPerKWh.trim().split(/\s+/);
             return parseFloat(tariffValues[0]) || 0;
         }
@@ -204,5 +295,12 @@ export const TariffChange = <T extends TariffChangeFormData>({
         return parseFloat(formData.costPerKWh) || 0;
     };
 
-    return { handleTariffChange, handleInputChange, getCalculationValue };
+    return {
+        handleTariffChange,
+        handleInputChange,
+        handleLoadSizeChange,
+        handleEfficiencyChange,
+        getCalculationValue,
+    };
 };
+
--- calc-front/components/calculators/WMComponent.tsx
+++ calc-front/components/calculators/WMComponent.tsx
@@ -1,4 +1,384 @@
-export const WMComponent = () => {
-    return <></>;
+"use client";
+
+import { MouseEvent, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { CalcInput } from "@/components/ui/calcInput";
+import { calculateWMConsumption } from "@/lib/calculators";
+import { useUnifiedStore } from "@/stores/stores";
+import {
+    TariffChange,
+    options,
+    efficiencyOptions,
+    loadSizeOptions,
+    icons,
+} from "@/components/TariffChange";
+import { Button } from "../ui/button";
+import { SelectInput } from "../ui/selectInput";
+import TooltipBtn from "../ui/tooltipBtn";
+import cn from "classnames";
+
+export interface FormData {
+    efficiencyClass: string;
+    weeklyLoads: number;
+    loadSize: string;
+    costPerKWh: string;
+    waterCostPerCubicMeter: string;
+    ageInYears: number;
+    nightRateFactor: number;
+    nightRateUsagePercentage: number;
+    city: string;
+    icon?: JSX.Element | JSX.Element[];
+    [key: string]: unknown;
+}
+
+interface CalculationResult {
+    yearly: {
+        totalEnergyConsumption: number;
+        energyCost: number;
+        totalWaterUsageLiters: number;
+        waterCost: number;
+        totalYearlyCost: number;
+    };
+    monthly: {
+        loads: number;
+        energyConsumption: number;
+        energyCost: number;
+        waterUsageLiters: number;
+        waterCost: number;
+        totalMonthlyCost: number;
+    };
+}
+
+const WMComponent = () => {
+    const [selectedCostPerKWh, setSelectedCostPerKWh] = useState<string>("single-zone");
+    const [selectedEfficiencyClass, setSelectedEfficiencyClass] = useState<string>("A++");
+    const [selectedLoadSize, setSelectedLoadSize] = useState<string>("medium");
+    const [formData, setFormData] = useState<FormData>({
+        efficiencyClass: "A++",
+        weeklyLoads: 1,
+        loadSize: "medium",
+        costPerKWh: "4.32",
+        waterCostPerCubicMeter: "13.45",
+        ageInYears: 2,
+        nightRateFactor: 1,
+        nightRateUsagePercentage: 0,
+        city: "",
+        icon: icons.day,
+    });
+    const [result, setResult] = useState<CalculationResult | null>(null);
+    const [isSelectOpen, setIsSelectOpen] = useState<boolean>(false);
+    const [isEfficiencySelectOpen, setIsEfficiencySelectOpen] = useState<boolean>(false);
+    const [isLoadSizeSelectOpen, setIsLoadSizeSelectOpen] = useState<boolean>(false);
+    const [isValid, setIsValid] = useState<boolean>(true);
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    const isInputDisabled = selectedCostPerKWh !== "three-zone";
+
+    const setCalculationDone = useUnifiedStore(state => state.setCalculationDone);
+    const setCalculationType = useUnifiedStore(state => state.setCalculationType);
+
+    const {
+        handleTariffChange,
+        handleInputChange,
+        handleEfficiencyChange,
+        handleLoadSizeChange,
+        getCalculationValue,
+    } = TariffChange({
+        selectedCostPerKWh,
+        setSelectedCostPerKWh,
+        setSelectedEfficiencyClass,
+        setSelectedLoadSize,
+        formData,
+        setFormData,
+        setIsValid,
+        setErrorMessage,
+    });
+
+    const calculateAndSetResult = (updatedInputs: FormData): void => {
+        const tariffForCalculation = getCalculationValue();
+
+        const result = calculateWMConsumption({
+            efficiencyClass: updatedInputs.efficiencyClass,
+            weeklyLoads: updatedInputs.weeklyLoads,
+            loadSize: updatedInputs.loadSize,
+            electricityCostPerKWh: tariffForCalculation * 100,
+            waterCostPerCubicMeter: parseFloat(updatedInputs.waterCostPerCubicMeter) * 100,
+            ageInYears: updatedInputs.ageInYears,
+            nightRateFactor: updatedInputs.nightRateFactor,
+            nightRateUsagePercentage: updatedInputs.nightRateUsagePercentage / 100,
+        });
+
+        setResult(result);
+        console.log(result);
+    };
+
+    const handleSubmit = (e: MouseEvent<HTMLButtonElement>): void => {
+        e.preventDefault();
+
+        if (selectedCostPerKWh === "three-zone") {
+            const isNumberValid = /^(?!$)(\d+(\.\d{0,2})?)$/.test(formData.costPerKWh);
+            if (!isNumberValid) {
+                setIsValid(false);
+                setErrorMessage("Введіть число до двох знаків після коми.");
+                return;
+            }
+        }
+
+        calculateAndSetResult(formData);
+        setCalculationDone(true);
+        setCalculationType("wm");
+    };
+
+    return (
+        <form className="flex flex-col xl:flex-row gap-4 xl:gap-16 text-lg xl:text-2xl h-full">
+            <div className="w-full bg-white rounded-b-[30px] px-4 pb-4 xl:px-0 xl:pb-0 xl:w-7/12 flex-shrink-0 flex flex-col justify-between">
+                <div className="relative mt-6 xl:mt-0">
+                    <TooltipBtn
+                        title="Пояснення показників"
+                        text="Вкажіть обсяг води, що використовується за місяць."
+                        buttonText="Зрозуміло"
+                    />
+                    <span>Потужність пральної машини:</span>
+                    <div className="mt-4 xl:mt-6 text-base xl:text-lg text-primary">
+                        <SelectInput
+                            options={efficiencyOptions.map((option) => ({
+                                label: option.label,
+                                value: option.value,
+                            }))}
+                            selectedValue={selectedEfficiencyClass}
+                            onChange={handleEfficiencyChange}
+                            isOpen={isEfficiencySelectOpen}
+                            setIsOpen={setIsEfficiencySelectOpen}
+                        />
+                    </div>
+                </div>
+
+                <div className="relative mt-6 xl:mt-0">
+                    <TooltipBtn
+                        title="Пояснення показників"
+                        text="Вкажіть обсяг води, що використовується за місяць."
+                        buttonText="Зрозуміло"
+                    />
+                    <span>Тариф на електроенергію:</span>
+                    <div className="flex flex-col items-center xl:flex-row mt-4 xl:mt-6 text-base xl:text-lg">
+                        <div>
+                            <SelectInput
+                                options={options.map((option) => ({
+                                    label: option.label,
+                                    value: option.value,
+                                }))}
+                                selectedValue={selectedCostPerKWh}
+                                onChange={handleTariffChange}
+                                isOpen={isSelectOpen}
+                                setIsOpen={setIsSelectOpen}
+                            />
+                        </div>
+
+                        <span className="hidden xl:block xl:mx-4">-</span>
+
+                        <div className="relative">
+                            <Input
+                                id="costPerKWhInput"
+                                type="text"
+                                value={formData.costPerKWh}
+                                onChange={handleInputChange}
+                                disabled={isInputDisabled}
+                                className={`px-4 py-4 w-full mt-4 xl:mt-0 xl:px-6 xl:py-6 rounded-2xl text-base xl:text-lg ${
+                                    isInputDisabled ? "bg-gray-200 cursor-not-allowed" : ""
+                                } ${!isValid ? "border-2 border-red-500" : ""}`}
+                            />
+                            <span className="absolute mt-2 xl:mt-0 right-4 top-1/2 transform -translate-y-1/2 whitespace-nowrap text-base xl:text-lg">
+                                грн/кВт
+                            </span>
+                            {formData.icon && Array.isArray(formData.icon) ? (
+                                <span className="absolute flex flex-row-reverse gap-12 xl:gap-16 items-center mt-2 xl:mt-0 left-[60px] xl:left-[72px] top-1/2 transform -translate-y-1/2">
+                                    {formData.icon.map((iconElement, index) => (
+                                        <span key={index}>{iconElement}</span>
+                                    ))}
+                                </span>
+                            ) : (
+                                <span className="absolute flex flex-row-reverse gap-12 xl:gap-16 items-center mt-2 xl:mt-0 left-[60px] xl:left-[72px] top-1/2 transform -translate-y-1/2">
+                                    {formData.icon}
+                                </span>
+                            )}
+                            {!isValid && (
+                                <div className="text-red-500 absolute text-sm mt-1">
+                                    {errorMessage}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+                {selectedCostPerKWh === "two-zone" && (
+                    <>
+                        <div className="relative mt-6 xl:mt-0">
+                            <TooltipBtn
+                                title="Пояснення показників"
+                                text="Вкажіть обсяг води, що використовується за місяць."
+                                buttonText="Зрозуміло"
+                            />
+                            <label htmlFor="nightRateUsagePercentage">
+                                Як часто використовуєте пральну машину вночі?
+                            </label>
+                            <div className="mt-4 xl:mt-6">
+                                <Input
+                                    id="nightRateUsagePercentage"
+                                    type="range"
+                                    min="0"
+                                    max="100"
+                                    value={formData.nightRateUsagePercentage}
+                                    onChange={handleInputChange}
+                                    className="w-[90%] px-4 py-4 xl:px-6 xl:py-6 rounded-2xl xl:text-lg"
+                                />
+                                <span className="absolute right-4 top-1/2 transform -translate-y-1/2 whitespace-nowrap text-base xl:text-lg">
+                                    {formData.nightRateUsagePercentage}%
+                                </span>
+                            </div>
+                        </div>
+                    </>
+                )}
+
+                <div className="relative mt-6 xl:mt-0">
+                    <TooltipBtn
+                        title="Пояснення показників"
+                        text="Вкажіть обсяг води, що використовується за місяць."
+                        buttonText="Зрозуміло"
+                    />
+                    <label htmlFor="waterCostPerCubicMeter">
+                        Який тариф на водопостачання використовуєте?
+                    </label>
+                    <div className="mt-4 xl:mt-6">
+                        <CalcInput
+                            id="waterCostPerCubicMeter"
+                            type="number"
+                            value={formData.waterCostPerCubicMeter}
+                            onChange={handleInputChange}
+                            unit="грн/м³"
+                            className="w-full px-4 py-4 xl:px-6 xl:py-6 rounded-2xl xl:text-lg"
+                        />
+                    </div>
+                </div>
+
+                <div className="relative mt-6 xl:mt-0">
+                    <TooltipBtn
+                        title="Пояснення показників"
+                        text="Вкажіть обсяг води, що використовується за місяць."
+                        buttonText="Зрозуміло"
+                    />
+                    <span>Об&apos;єм завантажених речей</span>
+                    <div className="mt-4 xl:mt-6 text-base xl:text-lg text-primary">
+                        <SelectInput
+                        options={loadSizeOptions.map((option) => ({
+                            label: option.label,
+                            value: option.value,
+                        }))}
+                        selectedValue={selectedLoadSize}
+                        onChange={handleLoadSizeChange}
+                        isOpen={isLoadSizeSelectOpen}
+                        setIsOpen={setIsLoadSizeSelectOpen}
+                        />
+                    </div>
+                </div>
+
+                <div className="relative mt-6 xl:mt-0">
+                    <TooltipBtn
+                        title="Пояснення показників"
+                        text="Вкажіть обсяг води, що використовується за місяць."
+                        buttonText="Зрозуміло"
+                    />
+                    <label htmlFor="weeklyLoads">
+                        Кількість використань пральної машини на тиждень:
+                    </label>
+                    <div className="mt-4 xl:mt-6">
+                        <CalcInput
+                            id="weeklyLoads"
+                            type="number"
+                            value={formData.weeklyLoads}
+                            onChange={handleInputChange}
+                            unit="разів"
+                            className="w-full px-4 py-4 xl:px-6 xl:py-6 rounded-2xl xl:text-lg"
+                        />
+                    </div>
+                </div>
+
+                <div className="relative mt-6 xl:mt-0">
+                    <TooltipBtn
+                        title="Пояснення показників"
+                        text="Вкажіть обсяг води, що використовується за місяць."
+                        buttonText="Зрозуміло"
+                    />
+                    <label htmlFor="ageInYears">Вік пральної машини в роках:</label>
+                    <div className="mt-4 xl:mt-6">
+                        <CalcInput
+                            id="ageInYears"
+                            type="number"
+                            value={formData.ageInYears}
+                            onChange={handleInputChange}
+                            unit="років"
+                            className="w-full px-4 py-4 xl:px-6 xl:py-6 rounded-2xl xl:text-lg"
+                        />
+                    </div>
+                </div>
+
+                <Button onClick={handleSubmit} size="xl" className="mt-6 py-4 xl:hidden text-lg">
+                    Розрахувати
+                </Button>
+            </div>
+
+            <div className="bg-white rounded-[30px] p-4 xl:p-0 flex flex-col justify-between">
+                <div className="flex flex-col gap-6 xl:gap-12">
+                    <div>
+                        <p className="mb-4">Пральна машина</p>
+                        <p className="mb-4 mt-10">Місячне споживання</p>
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.monthly.totalMonthlyCost.toFixed(2) || 0} грн/міс
+                        </p>
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.monthly.waterCost.toFixed(2) || 0} грн/міс
+                        </p>
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.monthly.waterUsageLiters.toFixed(2) || 0} літри/міс
+                        </p>       									
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.monthly.energyConsumption.toFixed(2) || 0} кВт·год/міс
+                        </p>
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.monthly.loads.toFixed(2) || 0} завантажень/міс
+                        </p>			
+                        <p className={cn("text-2xl xl:text-4xl font-semibold mb-2 xl:mb-4")}>
+                            {result?.monthly.energyCost.toFixed(2) || 0} грн/міс
+                        </p>			
+                    </div>
+
+                    <div className="border-t-2 pt-6 border-black xl:border-none xl:pt-0">
+                        <p className="mb-4">Річне споживання</p>
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.yearly.totalYearlyCost.toFixed(2) || 0} грн/рік
+                        </p>
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.yearly.waterCost.toFixed(2) || 0} грн/рік
+                        </p>
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.yearly.totalWaterUsageLiters.toFixed(2) || 0} літри/рік
+                        </p>			
+                        <p className="text-lg xl:text-xl text-gray-600">
+                            {result?.yearly.totalEnergyConsumption.toFixed(2) || 0} кВт·год/рік
+                        </p>
+                        <p className={cn("text-2xl xl:text-4xl font-semibold mb-2 xl:mb-4")}>
+                            {result?.yearly.energyCost.toFixed(2) || 0} грн/рік
+                        </p>
+                    </div>
+                </div>
+
+                <div>
+                    <Button size="xl" className="hidden xl:flex xl:text-2xl w-full" onClick={handleSubmit}>
+                        Розрахувати
+                    </Button>
+                </div>
+            </div>
+        </form>
+    );
 };
+
 export default WMComponent;
+
