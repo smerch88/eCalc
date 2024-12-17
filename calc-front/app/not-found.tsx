cda245dd760e9dad85340a73353d9e53c5125566
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-12-18 01:25:19 +0200

    added not found page

--- calc-front/app/not-found.tsx
+++ calc-front/app/not-found.tsx
@@ -2,11 +2,12 @@ import Link from 'next/link';
 
 export default function NotFound() {
     return (
-        <div className="h-full flex flex-col items-center mt-40">
-            <h1>Not found – 404!</h1>
-            <div>
-                <Link href="/">Go back to Home</Link>
-            </div>
+        <div className="container py-80 leading-9 flex flex-col justify-center items-center gap-y-4">
+            <h1 className="font-bold text-9xl">404</h1>
+            <h2 className="text-3xl font-normal">Page Not Found</h2>
+            <Link href="/" className="text-xl font-normal text-[#5a71eb]">
+                Go back home
+            </Link>
         </div>
     );
 }
