5b23979b26318babdd958b8ca52b7aaebe06eea4
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-12-18 01:25:59 +0200

    added contacts page

--- calc-front/page-sections/ContactsSection.tsx
+++ calc-front/page-sections/ContactsSection.tsx
@@ -1,60 +1,59 @@
-import cn from 'classnames';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import { contacts } from '@/lib/contacts';
 
 export const ContactsSection = () => {
     return (
-        <div className={cn('mt-40')}>
-            <div className="px-4 xl:container">
-                <h1 className="text-[32px] font-bold leading-10 xl:mb-12 xl:text-5xl">Контакти</h1>
-                <p className="text-lg xl:text-2xl xl:pr-0 mb-4 xl:mb-12">
-                    Ми завжди раді отримати від вас повідомлення! Залишайте свої запитання або
-                    коментарі, і ми обов&apos;язково відповімо.
-                </p>
-                <h2 className="text-2xl xl:text-4xl font-bold xl:font-semibold pb-4 xl:pb-6">
-                    Зв&apos;яжіться з нами
-                </h2>
-                <ul className="text-lg xl:text-2xl">
-                    <li>
-                        <strong>Електронна пошта:</strong>{' '}
-                        <Link href="mailto:info@yourproject.com" className="text-blue-500">
-                            support@e-calc.tech
-                        </Link>
+        <div className="px-4 xl:container mx-auto">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                {contacts.map((contact, index) => (
+                    <li
+                        key={index}
+                        className="flex flex-col bg-white rounded-xlg shadow-md p-6 h-60 xl:h-72"
+                    >
+                        <div className="flex items-center mb-8">
+                            <Link
+                                href={contact.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex items-center mr-2"
+                            >
+                                <span
+                                    className={`flex justify-center lg:hidden items-center w-10 h-10 rounded-2xl ${
+                                        index === 0 ? 'bg-black' : 'bg-blue-500'
+                                    }`}
+                                >
+                                    <Image
+                                        width={16}
+                                        height={16}
+                                        src={contact.icon}
+                                        alt={contact.alt}
+                                    />
+                                </span>
+                                <span
+                                    className={`hidden justify-center lg:flex items-center w-11 h-11 rounded-2xl ${
+                                        index === 0 ? 'bg-black' : 'bg-blue-500'
+                                    }`}
+                                >
+                                    <Image
+                                        width={24}
+                                        height={24}
+                                        src={contact.icon}
+                                        alt={contact.alt}
+                                    />
+                                </span>
+                            </Link>
+                            <strong className="text-lg xl:text-2xl text-primary">
+                                {contact.title}
+                            </strong>
+                        </div>
+                        <p className="text-lg xl:text-2xl font-normal text-primary">
+                            {contact.description}
+                        </p>
                     </li>
-                    <li>
-                        <strong>LinkedIn (Founder):</strong>{' '}
-                        <Link
-                            href="https://www.linkedin.com/in/arsenii-maksymenko"
-                            className="text-blue-500"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Перейти до профілю
-                        </Link>
-                    </li>
-                    <li>
-                        <strong>LinkedIn (Дизайнер):</strong>{' '}
-                        <Link
-                            href="https://www.linkedin.com/in/svitlana-synytsia/"
-                            className="text-blue-500"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Перейти до профілю дизайнера
-                        </Link>
-                    </li>
-                    {/* <li>
-                        <strong>LinkedIn (Розробник):</strong>{' '}
-                        <Link
-                            href="https://www.linkedin.com/in/developer-linkedin"
-                            className="text-blue-500"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Перейти до профілю розробника
-                        </Link>
-                    </li> */}
-                </ul>
-            </div>
+                ))}
+            </ul>
         </div>
     );
 };
