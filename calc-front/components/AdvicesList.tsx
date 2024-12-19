fde16f2bb415f1b0e3d6003191cbcac7177f7620
LongBeam <maxarmyk@gmail.com>
2024-12-19 11:57:44 +0200

    fix

--- calc-front/components/AdvicesList.tsx
+++ calc-front/components/AdvicesList.tsx
@@ -28,13 +28,12 @@ const AdvicesList = () => {
 
     if (!isCalculationDone) {
         return (
-
             <Scroll to="calculator-section" smooth={true} offset={10} duration={1500}>
                 <p className="text-xl xl:text-2xl text-center cursor-pointer text-placeholder">
                     Будь ласка, натисніть{' '}
                     <span className="text-black text-lg xl:text-2xl">&#34;Розрахувати&#34;</span>,
                     щоб отримати поради.
-                </span>
+                </p>
             </Scroll>
         );
     }
