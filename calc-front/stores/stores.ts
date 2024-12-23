455402a8fb7e661b7ae4b1c85dadbf839b5dd3c6
smerch88 <maxarmyk@gmail.com>
2024-12-24 00:57:41 +0200

    fix

--- calc-front/stores/stores.ts
+++ calc-front/stores/stores.ts
@@ -1,30 +1,36 @@
 import { UnifiedStoreState } from '@/types/common';
 import { create } from 'zustand';
 
-export const useUnifiedStore = create<UnifiedStoreState>(set => ({
-    location: localStorage.getItem('location') || 'Київ', 
-    setLocation: location => {
-        localStorage.setItem('location', location); 
-        set({ location });
-    },
-    tariffType: 'day',
-    setTariffType: type => set({ tariffType: type }),
-    boiler: null,
-    setBoiler: boiler => set({ boiler }),
-    advices: { boiler: [], light: [], wm: [], mw: [] },
-    setAdvices: (type, newAdvices) =>
-        set(state => {
-            if (state.advices[type] === newAdvices) {
-                return state;
+export const useUnifiedStore = create<UnifiedStoreState>(set => {
+    const isBrowser = typeof window !== 'undefined';
+
+    return {
+        location: isBrowser ? localStorage.getItem('location') || 'Київ' : 'Київ',
+        setLocation: location => {
+            if (isBrowser) {
+                localStorage.setItem('location', location);
             }
-            return {
-                advices: { ...state.advices, [type]: newAdvices },
-            };
-        }),
-    isCalculationDone: false,
-    setCalculationDone: (done: boolean) => set({ isCalculationDone: done }),
-    calculationType: 'boiler',
-    setCalculationType: type => set({ calculationType: type }),
-    activeTab: 'boiler',
-    setActiveTab: tab => set({ activeTab: tab }),
-}));
+            set({ location });
+        },
+        tariffType: 'day',
+        setTariffType: type => set({ tariffType: type }),
+        boiler: null,
+        setBoiler: boiler => set({ boiler }),
+        advices: { boiler: [], light: [], wm: [], mw: [] },
+        setAdvices: (type, newAdvices) =>
+            set(state => {
+                if (state.advices[type] === newAdvices) {
+                    return state;
+                }
+                return {
+                    advices: { ...state.advices, [type]: newAdvices },
+                };
+            }),
+        isCalculationDone: false,
+        setCalculationDone: (done: boolean) => set({ isCalculationDone: done }),
+        calculationType: 'boiler',
+        setCalculationType: type => set({ calculationType: type }),
+        activeTab: 'boiler',
+        setActiveTab: tab => set({ activeTab: tab }),
+    };
+});
