11da885e2e38213c8581cac4a52fe87ddc7d1620
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-12-24 18:51:01 +0200

    add new footer items; terms/policy pages

---
+++ calc-front/page-sections/TermsSection.tsx
@@ -0,0 +1,150 @@
+export default function TermsSection() {
+    return (
+        <section className="mt-40 px-4 xl:container">
+            <h1 className="text-[30px] font-bold leading-10 xl:mb-12 xl:text-5xl mb-8">
+                Умови та Положення
+            </h1>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Останнє оновлення: 24 грудня 2024 року
+            </p>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Будь ласка, уважно прочитайте ці умови та положення перед використанням нашого
+                сервісу.
+            </p>
+            <h2 className="text-2xl xl:text-4xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Тлумачення та Визначення
+            </h2>
+            <h3 className="text-xl xl:text-2xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Тлумачення
+            </h3>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Слова, перша літера яких написана з великої літери, мають значення, визначене в
+                наступних умовах.
+            </p>
+
+            <h3 className="text-xl xl:text-2xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Визначення
+            </h3>
+            <ul>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    <strong>Афілійована особа:</strong> суб&apos;єкт, який контролює, контролюється
+                    або перебуває під спільним контролем з боку сторони.
+                </li>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    <strong>Країна:</strong> Україна.
+                </li>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    <strong>Компанія:</strong> еКалькулятор.
+                </li>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    <strong>Пристрій:</strong> будь-який пристрій, що може отримати доступ до
+                    Сервісу.
+                </li>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    <strong>Умови та Положення:</strong> ці Умови та Положення.
+                </li>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    <strong>Третя сторона соціальних медіа:</strong> означає будь-які послуги або
+                    контент (включаючи дані, інформацію, продукти чи послуги), надані третьою
+                    стороною, які можуть бути відображені, включені або доступні через Сервіс.
+                </li>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    <strong>Сервіс:</strong> веб-сайт доступний за адресою{' '}
+                    <a href="mailto:support@e-calc.tech">support@e-calc.tech</a>.
+                </li>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    <strong>Ви:</strong> особа, яка отримує доступ до Сервісу або використовує його.
+                </li>
+            </ul>
+
+            <h2 className="text-2xl xl:text-4xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Визнання
+            </h2>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Ці Умови регулюють використання цього Сервісу. Використовуючи сервіс, ви
+                погоджуєтеся з цими умовами. Якщо ви не погоджуєтеся, не використовуйте Сервіс.
+            </p>
+
+            <h3 className="text-xl xl:text-2xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Посилання на Інші Веб-сайти
+            </h3>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Наш Сервіс може містити посилання на веб-сайти або сервіси третіх сторін, які не
+                належать і не контролюються Компанією.
+            </p>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Компанія не має контролю над змістом, політиками конфіденційності чи практиками
+                будь-яких веб-сайтів або сервісів третіх сторін. Ви також визнаєте і погоджуєтеся з
+                тим, що Компанія не несе відповідальності за будь-які збитки чи втрати, які можуть
+                бути завдані внаслідок використання або покладання на такі змісти, товари чи
+                послуги, доступні на таких веб-сайтах або сервісах.
+            </p>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Ми настійно рекомендуємо Вам ознайомитися з умовами та політиками конфіденційності
+                будь-яких веб-сайтів або сервісів третіх сторін, які Ви відвідуєте.
+            </p>
+            <h3 className="text-xl xl:text-2xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Припинення
+            </h3>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Ми можемо терміново припинити або призупинити Ваш доступ без попереднього
+                повідомлення чи відповідальності з будь-якої причини, включаючи без обмежень
+                порушення цих Умов і Положень.
+            </p>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Після припинення Ваше право використовувати Сервіс припиняється негайно.
+            </p>
+
+            <h3 className="text-xl xl:text-2xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Відмова від Гарантій
+            </h3>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Сервіс надається Вам &quot;ЯК Є&quot; і &quot;ЯК ДОСТУПНО&quot; без жодних гарантій.
+                Максимально допустимо в рамках чинного законодавства Компанія відмовляється від усіх
+                гарантій стосовно Сервісу.
+            </p>
+
+            <h3 className="text-xl xl:text-2xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Застосовне Право
+            </h3>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Закони Країни регулюють ці Умови та Ваше використання Сервісу.
+            </p>
+
+            <h3 className="text-xl xl:text-2xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Вирішення Спорів
+            </h3>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Якщо у Вас виникли питання чи спори щодо Сервісу, Ви погоджуєтеся спочатку
+                спробувати вирішити спір неформально шляхом зв&apos;язку з Компанією.
+            </p>
+
+            <h3 className="text-xl xl:text-2xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Зміни в Цих Умовах
+            </h3>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Ми залишаємо за собою право на власний розсуд змінювати ці Умови в будь-який час.
+                Якщо зміни є суттєвими ми зробимо все можливе для того щоб повідомити про це
+                принаймні за 30 днів до набуття чинності новими умовами.
+            </p>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Продовжуючи отримувати доступ до нашого Сервісу після того як ці зміни стануть
+                чинними ви погоджуєтеся дотримуватись нових умов. Якщо ви не погоджуєтеся з новими
+                умовами повністю або частково — будь ласка припиніть використовувати веб-сайт і
+                сервіс.
+            </p>
+
+            <h2 className="text-2xl xl:text-4xl font-bold xl:font-semibold pb-4 xl:pb-6">
+                Контакти
+            </h2>
+            <p className="text-base xl:text-xl mb-4 xl:mb-12">
+                Якщо у вас є запитання щодо цих Умов і Положень, ви можете зв&apos;язатися з нами:
+            </p>
+            <ul>
+                <li className="text-base xl:text-xl mb-4 xl:mb-12">
+                    Електронна пошта: support@e-calc.tech
+                </li>
+            </ul>
+        </section>
+    );
+}
